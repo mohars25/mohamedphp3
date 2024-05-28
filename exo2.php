77f951b054b87a88161148cd634f347704328711
Mohamed <162590718+mohars25@users.noreply.github.com>
2024-05-28 10:33:04 +0200

    Update exo2.php

--- exo2.php
+++ exo2.php
@@ -6,6 +6,8 @@
     Réaliser un algorithme permettant d’afficher le tableau HTML suivant (notez que le nom du pays
     s’affichera en majuscule et que le tableau est trié par ordre alphabétique du nom de pays) grâce à
     une fonction personnalisée.
+
+    
     Vous devrez appeler la fonction comme suit : afficherTableHTML($capitales);
     Affichage
     Pays Capitale
